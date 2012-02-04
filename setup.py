28b1f14dc2b1a6e0c25b50038e60f7cfea427da0
Kevin Huang <kevin@prismskylabs.com>
2012-02-03 16:34:24 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='pyawschart',
+    name='pythonawschart',
     version='0.2',
-    packages=['pyawschart',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    packages=['pythonawschart',],
+    license='',
     long_description='Python library for generating charts for Amazon Web Services based on Amazon CloudWatch data.',
 )
